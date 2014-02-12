222116991a49ec11b39c02e31772cf8c68492521
Andrea Stagi <stagi.andrea@gmail.com>
2014-02-12 13:09:58 +0100

    Added autodetect sample

---
+++ autodetect.py
@@ -0,0 +1,21 @@
+from imgburner import Burner
+import threading, time
+
+class AutoDetectThread(threading.Thread):
+    def run(self):
+        burner = Burner()
+        while True:
+            current_devices = burner.list_devices()
+            time.sleep(1)
+            rescanned_devices = burner.list_devices()
+            removed_devices = [x for x in current_devices if x not in rescanned_devices]
+            added_devices = [x for x in rescanned_devices if x not in current_devices]
+            if len(removed_devices) > 0:
+                for removed_device in removed_devices:
+                    print "REMOVED " + removed_device['DeviceIdentifier']
+            if len(added_devices) > 0:
+                for added_device in added_devices:
+                    print "ADDED " + added_device['DeviceIdentifier']
+
+auto_detect_thread = AutoDetectThread()
+auto_detect_thread.start()
